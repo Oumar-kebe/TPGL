11ec97d6d3f16ec255ab2aaa8ffc8ff95248d9d0
Oumar-kebe <oumarkebe20032003@gmail.com>
2025-09-29 10:16:53 +0000

    suite test majFinDeTour

--- src/test/java/re/forestier/edu/UnitTests.java
+++ src/test/java/re/forestier/edu/UnitTests.java
@@ -2,11 +2,13 @@ package re.forestier.edu;
 
 import org.junit.jupiter.api.*;
 import re.forestier.edu.rpg.player;
+import re.forestier.edu.rpg.UpdatePlayer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class UnitTests {
 
@@ -29,5 +31,49 @@ public class UnitTests {
         }
         fail();
     }
+
+    @Test
+    @DisplayName("Test MajFinDeTour: Joueur KO")
+    void testMajFinTourJKO(){
+        player p = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());  
+        p.currenthealthpoints = 0;
+        p.healthpoints = 100;
+        UpdatePlayer.majFinDeTour(p);
+   }
+
+   @Test
+   @DisplayName("")
+   void testMajFinTour(){
+        player p = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());  
+        p.currenthealthpoints = 20;
+        p.healthpoints = 100;
+        UpdatePlayer.majFinDeTour(p);
+        assertEquals(21, p.currenthealthpoints);
+        
+        p = new player("Florian", "Grognak le barbare", "DWARF", 100, new ArrayList<>(Arrays.asList("Holy Elixir")));  
+        p.currenthealthpoints = 20;
+        p.healthpoints = 100;
+        UpdatePlayer.majFinDeTour(p);
+        assertEquals(22, p.currenthealthpoints);
+
+        p = new player("Florian", "Grognak le barbare", "ARCHER", 100, new ArrayList<>(Arrays.asList("Magic Bow")));  
+        p.currenthealthpoints = 20;
+        p.healthpoints = 100;
+        UpdatePlayer.majFinDeTour(p);
+        assertEquals(22, p.currenthealthpoints);
+
+        p = new player("Florian", "Grognak le barbare", "ARCHER", 100, new ArrayList<>(Arrays.asList("Magic Bow")));  
+        p.currenthealthpoints = 50;
+        p.healthpoints = 100;
+        UpdatePlayer.majFinDeTour(p);
+        assertEquals(50, p.currenthealthpoints);
+
+        
+
+        
+        
+     }
+
+
 
 }
